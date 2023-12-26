fea72d29df6d091aa7bc3799e4047ee3861910d0
utkarsh032 <utkarshraj525@gmail.com>
2023-12-26 14:14:44 +0530

    filter & pagination

--- food-stuff_client/src/pages/Shop/Menu.jsx
+++ food-stuff_client/src/pages/Shop/Menu.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Card from '../../components/Card'
+import { FaFilter } from 'react-icons/fa'
 
 const Menu = () => {
 
@@ -7,6 +8,8 @@ const Menu = () => {
   const [filteredItems, setFilteredItems] = useState([])
   const [selectedCategory, setSelectedCategory] = useState("all")
   const [sortOption, setSortOption] = useState("default ")
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(9);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,49 +32,57 @@ const Menu = () => {
     const filtered = category === "all" ? menu : menu.filter((item) => item.category === category)
     setFilteredItems(filtered)
     setSelectedCategory(category)
+    setCurrentPage(1)
   }
 
   // show all
   const showAll = () => {
     setFilteredItems(menu)
     setSelectedCategory("all")
+    setCurrentPage(1)
   }
 
   const handleSortChange = (option) => {
-    setSortOption(option)
+    setSortOption(option);
 
-    let sortedItems = [...filterItems]
+    // Logic for sorting based on the selected option
+    let sortedItems = [...filteredItems];
 
     switch (option) {
-      case 'A-Z':
-        sortedItems.sort((a, b) => a.name.localCompare(b.name))
+      case "A-Z":
+        sortedItems.sort((a, b) => a.name.localeCompare(b.name));
         break;
-
-      case 'Z-A':
-        sortedItems.sort((b, a) => b.name.localCompare(a.name))
+      case "Z-A":
+        sortedItems.sort((a, b) => b.name.localeCompare(a.name));
         break;
-
-
-      case 'low-to-high':
-        sortedItems.sort((a, b) => a.price - b.price)
+      case "low-to-high":
+        sortedItems.sort((a, b) => a.price - b.price);
         break;
-
-      case 'high-to-low':
-        sortedItems.sort((a, b) => b.price - a.price)
+      case "high-to-low":
+        sortedItems.sort((a, b) => b.price - a.price);
         break;
       default:
+        // Do nothing for the "default" case
         break;
     }
 
-    setFilteredItems(sortedItems)
+    setFilteredItems(sortedItems);
+    setCurrentPage(1);
+  };
 
-  }
+  // pagination 
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredItems.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
 
   return (
-    <div>
+    <div className='max-w-screen-2xl  mx-auto '>
       {/*menu banner*/}
-      <div className='max-w-screen-2xl  mx-auto xl:px-24 text-[#0E3E4E] bg-gradient-to-tr from-[#CFFDFB] to-[#AF85E4]'>
+      <div className='xl:px-24 text-[#0E3E4E] bg-gradient-to-tr from-[#CFFDFB] to-[#AF85E4]'>
         <div className='py-48 flex flex-col justify-center items-center gap-8'>
           {/*Text */}
           <div className='text-center px-4 space-y-7'>
@@ -87,7 +98,7 @@ const Menu = () => {
       <div className='section-container bg-gradient-to-br from-[#CFFDFB] to-[#AF85E4] '>
 
         {/*filter and sort*/}
-        <div className='flex justify-between text-[#0E3E4E]'>
+        <div className='flex flex-col md:flex-row flex-wrap md:justify-between items-center space-y-3 mb-8'>
           <div className='flex flex-row justify-start md:items-center md:gap-8 gap-4 text-[#0E3E4E]'>
             <button onClick={showAll} className={selectedCategory === "all" ? "active" : ""}> All </button>
             <button onClick={() => filterItems("salad")} className={selectedCategory === "salad" ? "active" : ""} >Salad</button>
@@ -97,19 +108,48 @@ const Menu = () => {
             <button onClick={() => filterItems("drinks")} className={selectedCategory === "drinks" ? "active" : ""} >Drinks</button>
           </div>
 
-          <div>filter</div>
+          {/*sorting*/}
+          <div className='flex justify-end  items-center gap-1 px-1 bg-[#FC6D87]'>
+            <div>
+              <FaFilter className='h-4 w-4 text-[#fff] ' />
+            </div>
+
+            <select name="sort" id='sort' onChange={(e) => handleSortChange(e.target.value)} value={sortOption} className='bg-[#FC6D87] text-[#fff] px-2 py-1 rounded-sm'>
+              <option value="default">Default</option>
+              <option value="A-Z">A-Z</option>
+              <option value="Z-A">Z-A</option>
+              <option value="low-to-high">Low to High</option>
+              <option value="high-to-low">High to Low</option>
+            </select>
+
+          </div>
         </div>
 
         {/* product card */}
+
         <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 py-6'>
-          {filteredItems.map((item) => (
+          {currentItems.map((item) => (
             <Card key={item._id} item={item} />
           ))}
         </div>
+      </div>
 
-      </div >
 
-    </div >
+      {/*pagination */}
+      <div className="flex justify-center bg-gradient-to-tr from-[#CFFDFB] to-[#AF85E4]  py-8 text-[#fff]">
+        {Array.from({ length: Math.ceil(filteredItems.length / itemsPerPage) }).map((_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => paginate(index + 1)}
+            className={`mx-1 px-3 py-1 rounded-full ${currentPage === index + 1 ? "bg-[#FC6D87] " : "bg-[#949292]"
+              }`}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+
+    </div>
   )
 }
 
