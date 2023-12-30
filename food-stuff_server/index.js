0e0352e66546c6ecdc3880b02c6affa0f6624f8c
utkarsh032 <utkarshraj525@gmail.com>
2023-12-30 22:38:41 +0530

    MongoDB connected

--- food-stuff_server/index.js
+++ food-stuff_server/index.js
@@ -1,15 +1,49 @@
 const express = require('express')
 const app = express()
-const cors = cors()
+const cors = require('cors')
 const port = process.env.PORT || 3000
 
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('FoodStuff Server!')
 })
+
+// mongodb.connect
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6w7eemv.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const menuCollections = client.db("foodStuff_client").collection('menu')
+    const cartCollections = client.db("foodStuff_client").collection('cartItems')
+
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollections.find().toArray()
+      res.send(result)
+    })
+    await client.db('admin').command({ ping: 1 })
+    console.log("MongoDB server connected successfully!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
