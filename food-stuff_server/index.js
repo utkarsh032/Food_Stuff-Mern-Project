a7fe379bb7fd5acf522cb9c1fff690fba812771c
utkarsh032 <utkarshraj525@gmail.com>
2024-03-22 20:18:46 +0530

    deploy

--- food-stuff_server/index.js
+++ food-stuff_server/index.js
@@ -1,43 +1,44 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
-const app = express()
-const port = process.env.PORT || 5000
-require('dotenv').config()
-
+const path = require('path');
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+const app = express();
+const port = process.env.PORT || 5000;
 
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.json())
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'food-stuff_client/dist')));
 
-// mongodb config mongoose
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@foodstuff.jgvytnl.mongodb.net/foodStuff?retryWrites=true&w=majority`).then(console.log("MongoDB connected")).catch((error) => console.log("MongoDB error: " + error))
+// MongoDB Connection
+mongoose
+  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@foodstuff.jgvytnl.mongodb.net/foodStuff?retryWrites=true&w=majority`)
+  .then(() => console.log('MongoDB connected'))
+  .catch((error) => console.error('MongoDB connection error:', error));
 
-// jwt authentication
-app.post('/jwt', async (req, res) => {
-  const user = req.body
+// JWT Authentication Endpoint
+app.post('/jwt', (req, res) => {
+  const user = req.body;
   const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, {
-    expiresIn: '1hr'
-  })
-  res.send({ token })
-})
+    expiresIn: '1hr',
+  });
+  res.send({ token });
+});
 
+// Routes
+const menuRoutes = require('./api/routes/menuRoutes');
+const cartRoutes = require('./api/routes/cartRoutes');
+const userRoutes = require('./api/routes/userRoutes');
+app.use('/menu', menuRoutes);
+app.use('/carts', cartRoutes);
+app.use('/users', userRoutes);
 
-
-// routes 
-const menuRoutes = require('./api/routes/menuRoutes')
-const cartRoutes = require('./api/routes/cartRoutes')
-const userRoutes = require('./api/routes/userRoutes')
-app.use('/menu', menuRoutes)
-app.use('/carts', cartRoutes)
-app.use('/users', userRoutes)
-
-// stripePayment
-
+// Stripe Payment Endpoint
 app.post('/create-payment-intent', async (req, res) => {
   try {
     const { price } = req.body;
@@ -58,8 +59,13 @@ app.post('/create-payment-intent', async (req, res) => {
   }
 });
 
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
-
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
